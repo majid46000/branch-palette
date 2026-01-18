4263788e79f9625a66c24a42ede67059e91b3f35
majid46000 <majidmajid0075@gmail.com>
2026-01-18 01:37:30 +0100

    Create blocked.js

---
+++ scripts/blocked.js
@@ -0,0 +1,14 @@
+export const BLOCKED_BRANCHES = [
+  "spam",
+  "adult",
+  "casino",
+  "betting",
+  "illegal"
+];
+
+export const BLOCKED_KEYWORDS = [
+  "free money",
+  "hack",
+  "crack",
+  "torrent"
+];
